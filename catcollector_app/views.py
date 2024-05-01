ee3152e2cc21b0c6908d0cd8c6be96f2e14f81f4
julibennett <julibennett4@gmail.com>
2024-04-30 21:12:46 -0700

    day 2 django models

--- catcollector_app/views.py
+++ catcollector_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Cat
 
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 # Create your views here.
 
 def home(request):
@@ -8,3 +13,16 @@ def home(request):
 
 def about(request):
     return render(request, 'cats/about.html')
+
+def cats_index(request):
+  cats = Cat.objects.all()
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'cats/index.html', {
+    'cats': cats
+  })
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
+
+
